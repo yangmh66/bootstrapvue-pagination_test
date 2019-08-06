543dbd8df735401683f2673e97c6438dfd451568
Ming-Hsiao Yang <yangmh66@gmail.com>
2019-08-06 20:27:18 +0800

    test vue pagination features

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,65 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="app" class="align-items-center">
+    <h2>{{title}}</h2>
+    <div class="row">
+      <div class="col-md-12">
+        <li v-for="item in posts" :key="item.id">{{ item.id }} : {{ item.title }}</li>
+      </div>
+    </div>
+    <br />
+    <b-pagination
+      size="md"
+      :total-rows="100"
+      v-model="currentPage"
+      :per-page="10"
+      @input="getPostData(currentPage)"
+      align="center"
+    ></b-pagination>
+    <br />
+    <div>currentPage: {{currentPage}}</div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+//Import axios for REST API calls
+import axios from "axios";
+//Import bootstrap CSS
+import "bootstrap/dist/css/bootstrap.css";
+//Import bootstrap vue CSS
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      title: "Vue.js Pagination Example With Bootstrap",
+      currentPage: 1,
+      limit: 10,
+      posts: []
+    };
+  },
+  methods: {
+    // Fetches posts when the component is created.
+    getPostData(currentPage) {
+      axios
+        .get(
+          "http://jsonplaceholder.typicode.com/posts?_page=" +
+            this.currentPage +
+            "&_limit=" +
+            this.limit
+        )
+        .then(response => {
+          console.log(response);
+          // JSON responses are automatically parsed.
+          this.posts = response.data;
+        })
+        .catch(e => {
+          this.errors.push(e);
+        });
+    }
+  },
+  mounted(currentPage) {
+    this.getPostData(currentPage);
+  }
+};
+</script>
